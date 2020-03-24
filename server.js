fa98c4254de005599fa54b45d9ac8590908cd146
Steven Stot <sstott09@yahoo.com>
2020-03-23 18:08:40 -0600

    updated api rout

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = process.env.MONGODB_URI;
 
 // Connect to Mongo
 mongoose
@@ -27,11 +27,12 @@ if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
